ce95e24ea1492528e4d26b688728f1595ffb7f24
Allex Rodrigues <allexxrodriguess@gmail.com>
2020-07-14 22:33:40 -0300

    Updates

---
+++ precache-manifest.363b0201a8432f5e5cd177936c2a23b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "610037d87b1ba9b9d512fa7d7bb2c212",
+    "url": "/Todo-React/index.html"
+  },
+  {
+    "revision": "f9dc028bb4741f594607",
+    "url": "/Todo-React/static/css/main.1d737649.chunk.css"
+  },
+  {
+    "revision": "f97076c0b94c9931b559",
+    "url": "/Todo-React/static/js/2.695de954.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-React/static/js/2.695de954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9dc028bb4741f594607",
+    "url": "/Todo-React/static/js/main.e65c2364.chunk.js"
+  },
+  {
+    "revision": "16729d3bd5752d4c3840",
+    "url": "/Todo-React/static/js/runtime-main.2a4c68fb.js"
+  }
+]);
